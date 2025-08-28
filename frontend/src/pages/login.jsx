295d63c504e6a8b5356ddfbbc61bbb253b6ad5c1
pamela <drodriguez64@col.mx>
2025-08-27 18:31:25 -0600

    vistas principales

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,18 @@
+// src/pages/login.jsx
+import React from "react";
+
+const Login = () => {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#dee2e6"
+    }}>
+      <h1>Login Funciona ✅</h1>
+    </div>
+  );
+};
+
+export default Login;
