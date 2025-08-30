2002a262331a7726cc225e2274b596b03a240052
nanchi1005 <nancy16laureano@gmail.com>
2025-08-30 12:08:32 -0600

    agregue una carpeta de estilos para las paginas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+// main es el punto de entrada de la aplicación
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
