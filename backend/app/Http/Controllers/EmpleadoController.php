c23819f3c7c36b09c74a9bd3935c9faaeeb8dd9c
JuanitoDev <jangelina@ucol.mx>
2025-09-08 07:11:57 -0600

    nancy

---
+++ backend/app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+
+
+class EmpleadoController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Empleado::all());
+    }
+
+    public function show($id)
+    {
+        $empleado = Empleado::find($id);
+        if (!$empleado) {
+            return response()->json(['error' => 'Empleado no encontrado'], 404);
+        }
+        return response()->json($empleado);
+    }
+}
