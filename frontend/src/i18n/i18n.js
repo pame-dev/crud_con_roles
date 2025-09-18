676f4886e99345e0e2205d98c841c070bfdbf433
pamela <drodriguez64@col.mx>
2025-09-18 17:58:30 -0600

    historial con todos los turnos, boton de traducir, nombre en currentTurnCard

---
+++ frontend/src/i18n/i18n.js
@@ -0,0 +1,15 @@
+// src/i18n/i18n.js
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { resources } from "./translations";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "es", // idioma por defecto
+    fallbackLng: "es",
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
