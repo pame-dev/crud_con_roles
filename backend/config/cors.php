422971ed4c7e5c77119be2d55d1de13efeba33d2
nanchi1005 <nancy16laureano@gmail.com>
2025-11-25 16:41:01 -0600

    agregue el axios en el archivo de config y elimine una linea de cors para los permisos del olvide mi contraseña, tambien agregue el boton de cancelar

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -23,7 +23,6 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://crud-con-roles.vercel.app',
-        'https://*.vercel.app',
         'https://pitline.site',
         'https://www.pitline.site'
     ],
